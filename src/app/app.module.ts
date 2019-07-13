9fbf3d955646a14b1c3472a0813509b05a365256
nimosaid <saidnimo123@gmail.com>
2019-07-13 19:31:41 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+// import { ReposComponent } from './repos/repos.component';
+// import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
+    // ReposComponent,
+    // UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
